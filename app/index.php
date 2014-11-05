890933980393f066d0754ceee0ca54bce5051f50
Juanma Orta <juanma.orta@ulabox.com>
2014-11-05 11:49:13 +0100

    Image generation with imagine/imagine

--- app/index.php
+++ app/index.php
@@ -1,3 +1,34 @@
 <?php
-phpinfo();
-?>
+ini_set("display_errors", 1);
+require_once '../vendor/autoload.php';
+
+$imagine = new Imagine\Gd\Imagine();
+
+$palette = new Imagine\Image\Palette\RGB();
+$size  = new Imagine\Image\Box(32, 10);
+$color_black = $palette->color('#000');
+$font = new Imagine\Gd\Font('./fonts/Roboto-Regular.ttf', 8, $color_black);
+
+
+$postalCodes = array(
+    '08001',
+    '08002',
+    '08003',
+    '08004',
+    '08005',
+    '08006',
+    '08007',
+    '08008',
+    '08009',
+    '08010'
+);
+
+for($i=0; $i<count($postalCodes); $i++) {
+    $image = $imagine->create($size, $color_black);
+    $image->draw()
+        ->text($postalCodes[$i], $font, new Imagine\Image\Point(1,1));
+
+    $image->save('./images/' . $postalCodes[$i] . '.png');
+}
+
+print "done!";
