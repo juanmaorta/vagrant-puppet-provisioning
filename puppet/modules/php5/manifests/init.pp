b4c29508cf72b763ac150695d04e45ef6f5327d8
Juanma Orta <juanma.orta@gmail.com>
2014-11-05 21:09:09 +0100

    Added mcrypt

--- puppet/modules/php5/manifests/init.pp
+++ puppet/modules/php5/manifests/init.pp
@@ -1,9 +1,16 @@
 # vagrant/puppet/modules/php/manifests/init.pp
 class php5 {
     # Install the php5-fpm, php5-cli and php5-mysql packages
-    package { ['php5-fpm','php5-cli','php5-mysql', 'php5-gd', 'php5-curl']:
+    package { ['php5-fpm','php5-cli','php5-mysql', 'php5-gd', 'php5-curl', 'php5-mcrypt']:
         ensure => present,
         require => Exec['apt-get update'],
+    }
+
+    # Symlink mcrypt module
+    exec { 'symlink_mcrypt':
+      command => 'ln -s /etc/php5/mods-available/mcrypt.ini /etc/php5/fpm/conf.d/mcrypt.ini',
+      path    => ['/bin', '/usr/bin'],
+      require => Service['php5-fpm'],
     }
 
     # Make sure php5-fpm is running
